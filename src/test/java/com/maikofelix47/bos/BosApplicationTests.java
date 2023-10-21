07c5e39993db9f60b2dca91ce726a4f1d4ce263c
maikofelix47 <maikofelix89@gmail.com>
2023-10-21 12:28:59 +0300

    Set up project

---
+++ src/test/java/com/maikofelix47/bos/BosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maikofelix47.bos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
